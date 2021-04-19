469dd6850fef7150ba1fe1230278c88bdeef2b5b
timothy okooboh <okoobohtimothy@gmail.com>
2021-04-19 07:37:58 +0100

    fix image path

--- src/projectList.js
+++ src/projectList.js
@@ -3,7 +3,7 @@ export const projectList = [
         projectName: "PEP Exams",
         projectDescription: `A quiz application (PWA) used by Nigerian Pharmacists 
         to prepare for the licensing examination.`,
-        projectImage: "pep1.png",
+        projectImage: "/pep1.png",
         projectUrl: "https://pepexam.herokuapp.com/",
         projectRoute: "/project/1"
     },
@@ -11,21 +11,21 @@ export const projectList = [
         projectName: "MyFastMeds",
         projectDescription: `An E-pharmacy application with e-commerce and online consultation features. It is
         still in development`,
-        projectImage: "myfastmeds-1.png",
+        projectImage: "/myfastmeds-1.png",
         projectUrl: "https://myfastmeds.herokuapp.com/",
         projectRoute: "/project/2"
     },
     {
         projectName: "Covid-19 Dashboard",
         projectDescription: `Covid-19 dashboard providing statistics of cases and deaths across different countries`,
-        projectImage: "covid-1.png",
+        projectImage: "/covid-1.png",
         projectUrl: "https://timothyokooboh.github.io/#/",
         projectRoute: "/project/3"
     },
     {
         projectName: "English Dictionary",
         projectDescription: `An english dictionary with text to speech functionality`,
-        projectImage: "dictionary-1.png",
+        projectImage: "/dictionary-1.png",
         projectUrl: "https://timothyokooboh.github.io/dictionary/#/",
         projectRoute: "/project/4"
     },
